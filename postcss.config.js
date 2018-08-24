0e6ce5ceaea0bbfa3c51f9b77fb23fed95ff2b7d
lrh <837233792@qq.com>
2018-08-24 14:49:20 +0800

    project finish

---
+++ postcss.config.js
@@ -0,0 +1,7 @@
+const autoprefixer = require('autoprefixer')
+
+module.exports = {
+  plugins: [
+    autoprefixer()  //  优化CSS代码，自动添加CSS前缀
+  ]
+}
--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const path = require('path')
 const webpack = require('webpack')
 const VueLoaderPlugin = require('vue-loader/lib/plugin')
 const HTMLPlugin = require('html-webpack-plugin')
+const ExtractPlugin = require('extract-text-webpack-plugin')
 
 const isDev = process.env.NODE_ENV === 'development'
 
@@ -9,7 +10,7 @@ const config = {
   target: 'web',
   entry: path.join(__dirname, 'src/index.js'),
   output: {
-    filename: 'bundle.js',
+    filename: 'bundle.[hash:8].js',
     path: path.join(__dirname, 'dist')
   },
   module: {
@@ -19,19 +20,8 @@ const config = {
         loader: 'vue-loader'
       },
       {
-        test: /\.css$/,
-        use: [
-          'style-loader',
-          'css-loader'
-        ]
-      },
-      {
-        test: /\.styl/,
-        use: [
-          'style-loader',
-          'css-loader',
-          'stylus-loader'
-        ]
+        test: /\.jsx$/,
+        loader: 'babel-loader'
       },
       {
         test: /\.(gif|jpg|jpeg|png|svg)$/,
@@ -60,6 +50,20 @@ const config = {
 }
 
 if (isDev) {
+  config.module.rules.push({
+      test: /\.styl/,
+      use: [
+        'style-loader',
+        'css-loader',
+        {
+          loader: 'postcss-loader',
+          options: {
+            sourceMap: true,
+          }
+        },
+        'stylus-loader'
+      ]
+  })
   config.devtool = '#cheap-module-eval-source-map'
   config.devServer = {
     port: 8000,
@@ -72,6 +76,37 @@ if (isDev) {
   config.plugins.push(
     new webpack.HotModuleReplacementPlugin(),
     new webpack.NoEmitOnErrorsPlugin()
+  )
+} else {
+  config.entry = {
+    app: path.join(__dirname, 'src/index.js'),
+    vendor: ['vue']
+  }
+  config.output.filename = '[name].[chunkhash:8].js',
+  config.module.rules.push({
+    test: /\.styl/,
+    use: ExtractPlugin.extract({
+      fallback: 'style-loader',
+      use: [
+        'css-loader',
+        {
+          loader: 'postcss-loader',
+          options: {
+            sourceMap: true,
+          }
+        },
+        'stylus-loader'
+      ]
+    })
+  })
+  config.plugins.push(
+    new ExtractPlugin('style.[contentHash:8].css'),
+    new webpack.optimize.CommonsChunkPlugin({
+      name: 'vendor'
+    }),
+    new webpack.optimize.CommonsChunkPlugin({
+      name: 'runtime'
+    })
   )
 }
 
